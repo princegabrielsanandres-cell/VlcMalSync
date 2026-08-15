f242d529db1f350df2ee62dc1e7ec40ab98a6278
princegabrielsanandres-cell <princegabrielsanandres@gmail.com>
2026-08-15 11:49:37 +0800

    Update Java and Kotlin compatibility settings
    Set Java and Kotlin compatibility to version 17.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,14 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-android {
+android {compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlinOptions {
+    jvmTarget = "17"
+}
     namespace = "com.example.vlcmasync"
     compileSdk = 35
     defaultConfig {
